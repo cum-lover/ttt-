3f4f35cb463e753c0f6c017ed6453e02c873415d
cum-lover <105255253+cum-lover@users.noreply.github.com>
2022-06-12 19:06:28 +0800

    Add files via upload

---
+++ custom_errors.py
@@ -0,0 +1,6 @@
+class Error(Exception):
+    pass
+
+class InvalidActionError(Error):
+    def __init__(self, action, board, message):
+        print('InvalidActionError: ', message, 'Action: ', action, 'on board: ', board)
